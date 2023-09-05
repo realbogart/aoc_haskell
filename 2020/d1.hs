7c5799361fb2d55b8fdfabf03218d5eb2b09167f
Johan Yngman <johan.yngman@gmail.com>
2023-09-05 22:44:25 +0200

    Solved 2020 day 1

--- 2020/d1.hs
+++ 2020/d1.hs
@@ -1,8 +1,18 @@
 import Distribution.Simple.Utils
+import Data.List
 -- module Main
 
+combinationsOfTwo :: [a] -> [[a]]
+combinationsOfTwo xs = [ [x,y] | (x:ys) <- tails xs, y <- ys ]
+
+combinationsOfThree :: [a] -> [[a]]
+combinationsOfThree xs = [ [x,y,z] | (x:ys) <- tails xs, (y:zs) <- tails ys, z <- zs ]
+
 d1 :: String -> IO ()
-d1 a = do 
-  let test = sum . map read . words $ a
-  print test
+d1 = do 
+  print . map product . filter ((==2020) . sum) . combinationsOfTwo . map read . words
+
+d1_2 :: String -> IO ()
+d1_2 = do 
+  print . map product . filter ((==2020) . sum) . combinationsOfThree . map read . words
 
