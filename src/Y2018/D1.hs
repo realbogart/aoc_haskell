ba2a81c37cd4c31f78af501649407f6be88a32f2
Johan Yngman <johan.yngman@gmail.com>
2023-09-11 18:51:16 +0200

    Using Set from container library

---
+++ src/Y2018/D1.hs
@@ -0,0 +1,31 @@
+module Y2018.D1 where
+
+import AoC
+import qualified Data.Set as Set
+
+parseInput :: Parser [Int]
+parseInput = do
+  many frequencyShift
+    where frequencyShift :: Parser Int
+          frequencyShift = do
+            sign <- (-1) <$ char '-' <|> 1 <$ char '+'
+            frequency <- decimal
+            delim <- choice [void $ string ", ", void eol, eof]
+            return $ sign * frequency
+
+partOneTests :: [(Text,Int)]
+partOneTests = [("+1, +1, +1",3),("+1, +1, -2",0),("-1, -2, -3",(-6))]
+
+partTwoTests :: [(Text,Int)]
+partTwoTests = [("+1, -1",0), ("+3, +3, +4, -2, -4",10), ("-6, +3, +8, +5, -6",5),
+                ("+7, +7, -2, -7, -4",14)]
+
+partOne :: [Int] -> Int
+partOne = sum
+
+partTwo :: [Int] -> Int
+partTwo input = firstDuplicate Set.empty 0 (cycle input)
+  where firstDuplicate :: Set.Set Int -> Int -> [Int] -> Int
+        firstDuplicate previous acc (x:xs)  | Set.member acc previous = acc
+                                            | otherwise = firstDuplicate (Set.insert acc previous) (x+acc) xs
+
