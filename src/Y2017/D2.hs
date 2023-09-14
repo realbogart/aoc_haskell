31cde3b07155cbcaf5166869a7d99d189f996fee
Johan Yngman <johan.yngman@gmail.com>
2023-09-14 22:06:43 +0200

    Solved Y2017 D2

--- src/Y2017/D2.hs
+++ src/Y2017/D2.hs
@@ -8,6 +8,13 @@ parseInput = parseLineSeparated row
   where row = some (decimal <* optional hspace1)
 
 partOneTests = [("5 1 9 5\n7 5 3\n2 4 6 8", 18)]
+partTwoTests = [("5 9 2 8\n9 4 7 3\n3 8 6 5", 9)]
 
-partOne :: [[Int]] -> Int
-partOne = sum . map sum
+partOne = sum . map minMaxDiff 
+  where minMaxDiff :: [Int] -> Int
+        minMaxDiff xs = abs $ subtract (minimum xs) (maximum xs)
+
+partTwo = sum . map (sum . keepDivisable)
+  where keepDivisable = map fst . filter (\(q,r) -> r == 0) . map (uncurry quotRem) . twoPerms
+        twoPerms xs = [(x,y) | x <- xs, y <- xs, x/=y]
+
