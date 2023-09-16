e1c41c7d2fee0b5e2a5894ab1320ebc0a068dae5
Johan Yngman <johan.yngman@gmail.com>
2023-09-16 23:52:41 +0200

    Solved Y2019 D2

--- src/Y2019/D2.hs
+++ src/Y2019/D2.hs
@@ -1,10 +1,11 @@
 module Y2019.D2 where
 
 import AoC
+import Data.Vector qualified as V
 
 default (Text, Int)
 
-parseInput = some xs
+parseInput = V.fromList <$> some xs
   where xs :: Parser Int 
         xs = do x <- decimal
                 _ <- optional (char ',')
@@ -12,5 +13,16 @@ parseInput = some xs
 
 partOneTests = [("1,9,10,3,2,3,11,0,99,30,40,50",3500)]
 
-partOne :: [Int] -> Int
-partOne _ = 654
+partOne :: V.Vector Int -> Int
+partOne = execute 0 
+  where execute pc v  | op == 99 = (V.!) v 0
+                      | otherwise = execute (pc + 4) (V.update v (V.fromList [(target, op_f arg1 arg2)]))
+          where op_f = case op of
+                            1 -> (+)
+                            2 -> (*)
+                            _ -> error "Invalid op"
+                op = (V.!) v            (pc + 0)
+                arg1 = (V.!) v ((V.!) v (pc + 1))
+                arg2 = (V.!) v ((V.!) v (pc + 2))
+                target = (V.!) v        (pc + 3)
+                
