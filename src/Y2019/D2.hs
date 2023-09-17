c69f9ea15147ccce9c57cdaa2e4221f531c4e1fe
Johan Yngman <johan.yngman@gmail.com>
2023-09-17 09:06:18 +0200

    Tweaks

--- src/Y2019/D2.hs
+++ src/Y2019/D2.hs
@@ -16,13 +16,14 @@ partOneTests = [("1,9,10,3,2,3,11,0,99,30,40,50",3500)]
 partOne :: V.Vector Int -> Int
 partOne = execute 0 
   where execute pc v  | op == 99 = (V.!) v 0
-                      | otherwise = execute (pc + 4) (V.update v (V.fromList [(target, op_f arg1 arg2)]))
-          where op_f = case op of
+                      | otherwise = execute (pc + 4) execute_op
+          where execute_op = V.update v (V.fromList [(target, op_f arg1 arg2)])
+                op_f = case op of
                             1 -> (+)
                             2 -> (*)
                             _ -> error "Invalid op"
-                op = (V.!) v            (pc + 0)
-                arg1 = (V.!) v ((V.!) v (pc + 1))
-                arg2 = (V.!) v ((V.!) v (pc + 2))
-                target = (V.!) v        (pc + 3)
+                op      = (V.!) v           (pc + 0)
+                arg1    = (V.!) v ((V.!) v  (pc + 1))
+                arg2    = (V.!) v ((V.!) v  (pc + 2))
+                target  = (V.!) v           (pc + 3)
                 
