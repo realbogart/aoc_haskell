29aabefe2c923cfb928ee9f32e9bbea77f2c38e8
Johan Yngman <johan.yngman@gmail.com>
2023-12-11 21:57:50 +0100

    Y2023 Part 2 wip

--- src/Y2023/D5.hs
+++ src/Y2023/D5.hs
@@ -7,12 +7,13 @@ default (Text, Int)
 input = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4"
 
 partOneTests = [(input, 35)]
+partTwoTests = [(input, 46)]
 
 data Range = Range
   { destination :: Int
   , source :: Int
   , count :: Int
-  } deriving (Show)
+  } deriving (Show, Eq, Ord)
 
 data Map = Map
   { name :: [Char] 
@@ -54,8 +55,67 @@ mapValue v m  | num_matches == 0 = v
         range = head matchingRanges
         num_matches = length matchingRanges
 
+getRangeIntersection :: Range -> Range -> Maybe Range
+getRangeIntersection from to  | from.source + from.count <= to.source = Nothing
+                              | from.source >= to.source + to.count = Nothing
+                              | from.source <= to.source = Just $ Range 0 
+                                                                        to.destination
+                                                                        (right - to.source)
+                              | to.source <= from.source = Just $ Range 0
+                                                                        (to.destination + (from.source - to.source)) 
+                                                                        (right - from.source)
+                              | otherwise = error "Should not be here"
+                              where right = min (from.source + from.count) (to.source + to.count)
+
+sortRangesBySource :: [Range] -> [Range]
+sortRangesBySource = sortBy (\a b -> compare a.source b.source)
+
+getRangeIntersections :: Range -> [Range] -> [Range]
+getRangeIntersections target = mapMaybe (getRangeIntersection target)
+
+getRangeGaps :: Range -> [Range] -> [Range]
+getRangeGaps target ranges = getRangeIntersections target ( getFirst sorted_ranges ++ 
+                                                            getLast sorted_ranges ++
+                                                            mapMaybe getGap range_pairs)
+  where sorted_ranges = sortRangesBySource ranges
+        range_pairs = zip sorted_ranges (tail sorted_ranges)
+        getFirst :: [Range] -> [Range]
+        getFirst (f:_)  | target.source < f.source = [Range target.source target.source (f.source - target.source)]
+                        | otherwise = []
+        getFirst _ = []
+        getLast :: [Range] -> [Range]
+        getLast [] = []
+        getLast rs  | (target.source + target.count) > l_right = [Range l_right 
+                                                                        l_right 
+                                                                        (target.source + target.count - l_right)]
+                    | otherwise = []
+                where l = last rs
+                      l_right = l.source + l.count
+        getGap :: (Range, Range) -> Maybe Range
+        getGap (a,b)  | a.source + a.count < b.source = Just $ Range  (a.source + a.count) 
+                                                                      (a.source + a.count)
+                                                                      (b.source - (a.source + a.count))
+                      | otherwise = Nothing
+
+mapRange :: [Range] -> Map -> [Range]
+mapRange targets m = concatMap transformValueRange targets
+  where transformValueRange t = getRangeIntersections t m.ranges ++ 
+                                getRangeGaps t m.ranges
+
 applyMaps :: [Map] -> Int -> Int
 applyMaps maps v = foldl' mapValue v maps
 
+applyMapsToRange :: [Map] -> Range -> [Range]
+applyMapsToRange maps r = foldl' mapRange r maps
+
+getSeedRanges :: [Int] -> [Range]
+getSeedRanges = map getSeedRange . chunksOf 2
+  where getSeedRange [start, c] = Range 0 start c
+        getSeedRange _ = error "Invalid seed pair"
+
 partOne :: Almanac -> Int
 partOne a = minimum (map (applyMaps a.maps) a.seeds)
+
+partTwo :: Almanac -> Int
+partTwo a = minimum (concatMap (applyMapsToRange a.maps) seed_ranges)
+  where seed_ranges = getSeedRanges a.seeds
