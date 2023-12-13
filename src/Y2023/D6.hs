9b5d0ca2e40346afe28d71b5fc7249a1a108b1ad
Johan Yngman <johan.yngman@gmail.com>
2023-12-13 23:22:29 +0100

    Solved Y2023 D6 Part 2

--- src/Y2023/D6.hs
+++ src/Y2023/D6.hs
@@ -4,7 +4,10 @@ import AoC
 
 default (Int, Text)
 
-partOneTests = [("Time:      7  15   30\nDistance:  9  40  200", 288)]
+input = "Time:      7  15   30\nDistance:  9  40  200"
+
+partOneTests = [(input, 288)]
+partTwoTests = [(input, 71503)]
 
 data Race = Race
   { time :: Int
@@ -27,5 +30,14 @@ beatRecord race distance = distance > race.distance
 getBrokenRecords race = length $ filter id $ map (beatRecord race . getDistanceFromButtonTime race) button_durations
   where button_durations = [0..race.time]
 
+getBigRace :: [Race] -> Race
+getBigRace = g . foldl' f ("","")
+  where f (acc_time, acc_distance) race = (acc_time ++ show race.time, acc_distance ++ show race.distance)
+        g (acc_time, acc_distance) = Race (read acc_time) (read acc_distance)
+
 partOne :: [Race] -> Int
 partOne = product . map getBrokenRecords
+
+partTwo :: [Race] -> Int
+partTwo races = getBrokenRecords big_race
+  where big_race = getBigRace races
