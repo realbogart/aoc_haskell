28f43f3282a8c34c7a7255262fbe16ce33d85622
Johan Yngman <johan.yngman@gmail.com>
2023-12-14 22:10:34 +0100

    Solved Y2023 D7 Part 2

--- src/Y2023/D7.hs
+++ src/Y2023/D7.hs
@@ -17,7 +17,7 @@ data Hand = Hand
 cards hand = hand.cards
 bid hand = hand.bid
 
-data HandType = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
+data HandType = EmptyHand | HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind
   deriving (Show, Eq, Ord)
 
 data HandInfo = HandInfo
@@ -33,6 +33,10 @@ getCardValue 'J' = 11
 getCardValue 'T' = 10
 getCardValue c = digitToInt c
 
+getCardValueJoker :: Char -> Int
+getCardValueJoker 'J' = 1
+getCardValueJoker c = getCardValue c
+
 getHandType :: Hand -> HandType
 getHandType hand = case card_groups of 
                       (5:_) ->    FiveOfAKind
@@ -41,22 +45,43 @@ getHandType hand = case card_groups of
                       (3:_) ->    ThreeOfAKind
                       (2:2:_) ->  TwoPair
                       (2:_) ->    OnePair
+                      [] ->       EmptyHand
                       _ ->        HighCard
   where cards_sorted = sort hand.cards
         card_groups = sortBy (comparing Down) $ map length $ group cards_sorted
 
+upgradeHandTypeWithJoker EmptyHand = HighCard
+upgradeHandTypeWithJoker HighCard = OnePair
+upgradeHandTypeWithJoker OnePair = ThreeOfAKind
+upgradeHandTypeWithJoker TwoPair = FullHouse
+upgradeHandTypeWithJoker ThreeOfAKind = FourOfAKind
+upgradeHandTypeWithJoker FullHouse = error "Can't have a full house and a joker at the same time!"
+upgradeHandTypeWithJoker FourOfAKind = FiveOfAKind
+upgradeHandTypeWithJoker FiveOfAKind = error "Can't have five of a kind and a joker at the same time!"
+
+getHandTypeJoker :: Hand -> HandType
+getHandTypeJoker hand = last $ take (number_of_jokers + 1) $ iterate upgradeHandTypeWithJoker base_hand_type
+  where cards_without_jokers = filter (/= 'J') hand.cards
+        number_of_jokers = 5 - length cards_without_jokers
+        base_hand_type = getHandType (Hand cards_without_jokers 0)
+
 getHandInfo :: Hand -> HandInfo
 getHandInfo hand = HandInfo (getHandType hand) (map getCardValue hand.cards)
+
+getHandInfoJoker :: Hand -> HandInfo
+getHandInfoJoker hand = HandInfo (getHandTypeJoker hand) (map getCardValueJoker hand.cards)
 
 parseInput = parseLineSeparated parseHand
   where parseHand = do
           cs <- count 5 latin1Char <* space
           Hand cs <$> decimal
 
+ranks = [1..]
+
 partOne :: [Hand] -> Int
 partOne hands = sum $ zipWith (*) (map bid hands_sorted) ranks
   where hands_sorted = sortBy (comparing getHandInfo) hands
-        ranks = [1..]
 
 partTwo :: [Hand] -> Int
-partTwo _ = 54
+partTwo hands = sum $ zipWith (*) (map bid hands_sorted) ranks
+  where hands_sorted = sortBy (comparing getHandInfoJoker) hands
