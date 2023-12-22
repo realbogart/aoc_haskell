6cf6a5dab48c33569e89c5188b37c88955858229
Johan Yngman <johan.yngman@gmail.com>
2023-12-22 23:48:29 +0100

    Solved Y2023 D9 Part 2

--- src/Y2023/D9.hs
+++ src/Y2023/D9.hs
@@ -8,5 +8,14 @@ partOneTests = [("0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45", 114)]
 
 parseInput = parseLineSeparated (some parseSignedInteger)
 
+differences :: [Int] -> [Int]
+differences l = map (uncurry (flip (-))) pairs
+  where pairs :: [(Int, Int)]
+        pairs = zip l (tail l)
+
+pyramidNext :: [Int] -> Int
+pyramidNext l | all (== 0) l = 0
+              | otherwise = last l + pyramidNext (differences l)
+
 partOne :: [[Int]] -> Int
-partOne _ = 54
+partOne = sum . map pyramidNext
