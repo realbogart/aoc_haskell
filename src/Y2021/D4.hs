d2705852d93d3a38ef42223a261e73d78b28b889
Johan Yngman <johan.yngman@gmail.com>
2024-02-10 08:35:12 +0100

    Solve Y2021 D4

--- src/Y2021/D4.hs
+++ src/Y2021/D4.hs
@@ -24,5 +24,33 @@ parseInput = do
 
 partOneTests = [("7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7", 4512)]
 
+lanes :: [[(Int, Int)]]
+lanes = rows ++ columns
+  where rows = [[(x, y) | x <- [0..4]] | y <- [0..4]]
+        columns = [[(x, y) | y <- [0..4]] | x <- [0..4]]
+
+markBoard :: Int -> Grid Int -> Grid Int
+markBoard target board = Grid (V.fromList $ map (\x -> if x == target then -1 else x) $ V.toList board.grid) 5 5
+
+markBoards :: Int -> [Grid Int] -> [Grid Int]
+markBoards target = map (markBoard target)
+
+hasWon :: Grid Int -> Bool
+hasWon board = any (== True) $ map isLaneFilled lanes
+  where isLaneFilled :: [(Int, Int)] -> Bool
+        isLaneFilled l = all (== -1) $ map (getGridValue board) l 
+
+countScore :: Int -> Grid Int -> Int
+countScore n board = n * (sum $ filter (/= -1) $ V.toList board.grid)
+
+play :: Bingo -> Int
+play (Bingo [] _) = error "No winner"
+play (Bingo (n:ns) boards) 
+  | length winner_boards > 0 = head winner_scores
+  | otherwise = play (Bingo ns next_boards)
+    where next_boards = markBoards n boards
+          winner_boards = filter hasWon next_boards
+          winner_scores = map (countScore n) winner_boards
+
 partOne :: Bingo -> Int
-partOne _ = 4
+partOne = play
