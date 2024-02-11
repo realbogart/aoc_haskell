99dae6c4631f08f993428e8e0b674f670e0606ed
Johan Yngman <johan.yngman@gmail.com>
2024-02-11 09:56:43 +0100

    Formatting

--- src/Y2015/D5.hs
+++ src/Y2015/D5.hs
@@ -6,6 +6,8 @@ default (Text, Int)
 
 partOneTests = [("ugknbfddgicrmopn\naaa\njchzalrnumimnmhp\nhaegwjzuvuyypxyu\ndvszwmarrgswjxmb", 2)]
 
+partTwoTests = [("qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazucvgmuy", 2)]
+
 parseInput :: Parser [[Char]]
 parseInput = parseLineSeparated (some letterChar)
 
@@ -14,10 +16,20 @@ isNice s = (length vowels >= 3) && twice && not prohibited
   where
     vowels = filter (`elem` ("aeiou" :: [Char])) s
     twice = any ((> 1) . length) (group s)
-    prohibited =  isInfixOf "ab" s ||
-                  isInfixOf "cd" s ||
-                  isInfixOf "pq" s ||
-                  isInfixOf "xy" s
+    prohibited =
+      isInfixOf "ab" s
+        || isInfixOf "cd" s
+        || isInfixOf "pq" s
+        || isInfixOf "xy" s
+
+isNicer :: [Char] -> Bool
+isNicer s = pairTwice && repeatedChar
+  where
+    pairTwice = False
+    repeatedChar = False
 
 partOne :: [[Char]] -> Int
 partOne = length . filter isNice
+
+partTwo :: [[Char]] -> Int
+partTwo = length . filter isNicer
