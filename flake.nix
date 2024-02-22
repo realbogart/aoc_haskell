22616bda8460497608b1b994b3891a6a05f5439d
Johan Yngman <johan.yngman@gmail.com>
2024-02-22 21:30:06 +0000

    flake.nix wip

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Haskell development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [ haskell.compiler.ghcHEAD ];
+      shellHook = ''
+        exec $SHELL
+      '';
+    };
+  };
+}
+
