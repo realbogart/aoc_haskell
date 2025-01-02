274c228e82172aa3cc5cb2073a4b276bc9c2d443
Johan Yngman <johan.yngman@gmail.com>
2025-01-02 19:03:09 +0100

    haskell.nix rewrite WIP

--- flake.nix
+++ flake.nix
@@ -1,21 +1,44 @@
 {
-  description = "Haskell development environment";
+  description = "Solving Advent of Code problems using Haskell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url =
+      "github:nixos/nixpkgs/e12483116b3b51a185a33a272bf351e357ba9a99";
+    flake-utils.url = "github:numtide/flake-utils";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
   };
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = with pkgs; [ haskell.compiler.ghcHEAD ];
-      shellHook = ''
-        exec $SHELL
-      '';
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            benchit = final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc98";
+              shell.tools = {
+                cabal = { };
+                hlint = { };
+                haskell-language-server = { };
+              };
+              shell.buildInputs = with pkgs; [ nixpkgs-fmt ghcid ormolu ];
+            };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        flake = pkgs.benchit.flake { };
+      in flake // {
+        packages.default = flake.packages."benchit:exe:benchit";
+        nixconfig = {
+          substituters =
+            [ "s4://nix-cache-new?endpoint=ams3.digitaloceanspaces.com" ];
+          trusted-public-keys = [
+            "ams4.digitaloceanspaces.com:EwCAHHfy9VQ3OY7bOtlG9O/TNdAz79EBu7HSJdDcYXc="
+          ];
+        };
+      });
 }
-
