8738d9d187f596bfc032f73212eddbc29aec1d41
Johan Yngman <johan.yngman@gmail.com>
2025-12-21 22:13:33 +0100

    Fix

--- src/Y2025/D1.hs
+++ src/Y2025/D1.hs
@@ -15,7 +15,9 @@ partOneTests =
   ]
 
 partTwoTests =
-  [ ("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n", 6)
+  [ ("L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n", 6),
+    ("L151", 2),
+    ("R251", 3)
   ]
 
 turndial :: Int -> Int -> Int
@@ -27,15 +29,17 @@ turndial from offset
 
 passZero :: Int -> Int -> Int
 passZero input start =
-  if (abs input) > 100
-    then full_laps
-    else
-      if (offset < 0 || offset >= 100)
-        then 1
-        else 0
+  -- trace ((show start) ++ ":" ++ (show offset) ++ ":" ++ (show full_laps)) $
+  if (offset <= 0 || offset >= 100) && (start /= 0)
+    then 1 + full_laps
+    else full_laps
   where
-    offset = input + start
     full_laps = div (abs input) 100
+    leftover =
+      if input > 0
+        then input + (-full_laps * 100)
+        else input + (full_laps * 100)
+    offset = start + leftover
 
 partOne :: [Int] -> Int
 partOne = length . filter (== 0) . scanl turndial 50
