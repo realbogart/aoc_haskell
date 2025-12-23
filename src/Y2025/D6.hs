bf2e758caee2fcf1b6518c8cb57641dd558eb858
Johan Yngman <johan.yngman@gmail.com>
2025-12-23 21:42:54 +0100

    D6 2025 part 1

---
+++ src/Y2025/D6.hs
@@ -0,0 +1,28 @@
+module Y2025.D6 where
+
+import AoC
+import Data.List (foldl1', transpose)
+
+default (Text, Int)
+
+type Math = [([Int], Char)]
+
+parseInput :: Parser Math
+parseInput = do
+  rows <- parseLineSeparated (some parseInteger <* space) <* optional eol
+  operations <- some ((char '*' <|> char '+') <* space)
+  let cols = transpose rows
+  return $ zip cols operations
+
+getOperation :: Char -> (Int -> Int -> Int)
+getOperation '*' = (*)
+getOperation '+' = (+)
+getOperation _ = error "Invalid operation"
+
+evaluate :: ([Int], Char) -> Int
+evaluate (numbers, operation) = foldl1' (getOperation operation) numbers
+
+partOneTests = [("123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ", 4277556)]
+
+partOne :: Math -> Int
+partOne = sum . map evaluate
