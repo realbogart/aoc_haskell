a7c009585be0f27c6cd4c095d77ae8bca622769f
Johan Yngman <johan.yngman@gmail.com>
2025-12-23 21:49:54 +0100

    Refactor

--- src/Y2025/D6.hs
+++ src/Y2025/D6.hs
@@ -5,14 +5,15 @@ import Data.List (foldl1', transpose)
 
 default (Text, Int)
 
-type Math = [([Int], Char)]
+type Math = ([([Int], Char)], [([Int], Char)])
 
 parseInput :: Parser Math
 parseInput = do
   rows <- parseLineSeparated (some parseInteger <* space) <* optional eol
   operations <- some ((char '*' <|> char '+') <* space)
   let cols = transpose rows
-  return $ zip cols operations
+      part1 = zip cols operations
+  return (part1, part1)
 
 getOperation :: Char -> (Int -> Int -> Int)
 getOperation '*' = (*)
@@ -24,5 +25,10 @@ evaluate (numbers, operation) = foldl1' (getOperation operation) numbers
 
 partOneTests = [("123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ", 4277556)]
 
+partTwoTests = [("123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ", 3263827)]
+
 partOne :: Math -> Int
-partOne = sum . map evaluate
+partOne (a, _) = sum $ map evaluate a
+
+partTwo :: Math -> Int
+partTwo (_, b) = sum $ map evaluate b
