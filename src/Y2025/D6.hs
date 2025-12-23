ce6c9d3e33ced5ff29ac43d01589ad0d9eb18e1f
Johan Yngman <johan.yngman@gmail.com>
2025-12-23 23:56:16 +0100

    D6 2025 part 2

--- src/Y2025/D6.hs
+++ src/Y2025/D6.hs
@@ -18,10 +18,17 @@ type Math = [([Int], Char)]
 
 parseInput :: Parser Math
 parseInput = do
-  rows <- parseLineSeparated (some parseInteger <* space) <* optional eol
-  operations <- some ((char '*' <|> char '+') <* space)
+  rows <- parseLineSeparated (some (digitChar <|> char ' '))
+  operations_row <- some (char '*' <|> char '+' <|> char ' ')
   let cols = transpose rows
-  return $ zip cols operations
+      number_cols = map (filter isDigit) cols
+      number_cols_split = splitOn [""] number_cols
+      numbers = map (map getNumber) number_cols_split
+      operations = filter (/= ' ') operations_row
+  return $ zip numbers operations
+
+getNumber :: [Char] -> Int
+getNumber = read . filter isDigit
 
 getOperation :: Char -> (Int -> Int -> Int)
 getOperation '*' = (*)
@@ -38,5 +45,4 @@ partTwoTests = [("123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *  
 partOne :: Math -> Int
 partOne = sum . map evaluate
 
-partTwo :: Math -> Int
-partTwo = sum . map evaluate
+partTwo = partOne
