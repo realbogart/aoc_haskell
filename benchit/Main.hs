d477fcb1148802b11250954cc5bcbc38671f5bdf
Johan Yngman <johan.yngman@gmail.com>
2025-12-24 23:38:38 +0100

    D7 2025 part 2

--- benchit/Main.hs
+++ benchit/Main.hs
@@ -1,16 +1,8 @@
 module Main (main) where
 
 import AoC
--- import Y2023.D12
-import Y2024.D17
+import Y2025.D7
 
 main :: IO ()
 main = do
-  parseTestAndSolve parseInput partTwo partTwoTests "src/Y2024/D17.txt"
-
--- main :: IO ()
--- main = do
---   parseTestAndSolve parseInput partTwo partTwoTests "src/Y2023/D12.txt"
---   putStrLn "BENCH!"
-
--- ghcid --command="cabal repl" --test "AoC.parseTestAndSolve $1.$2.parseInput $1.$2.$3 $1.$2.$3Tests \"$4\""
+  parseTestAndSolve parseInput partTwo partTwoTests "src/Y2025/D7.txt"
--- src/Y2025/D7.hs
+++ src/Y2025/D7.hs
@@ -1,13 +1,18 @@
 module Y2025.D7 where
 
 import AoC
+import Data.HashMap.Strict qualified as HMS
+import Data.IORef
 import Data.List (nub)
 import Data.Text qualified as T
+import System.IO.Unsafe (unsafePerformIO)
 import Text.Megaparsec (MonadParsec (takeWhileP), someTill)
 
 default (Text, Int)
 
 type Input = (Int, [[Int]])
+
+type SplitMap = HMS.HashMap (Int, Int) Int
 
 parseInput :: Parser Input
 parseInput = do
@@ -21,9 +26,11 @@ parseInput = do
     parseSplitter :: Parser Int
     parseSplitter = length <$> some (char '.') <* optional (char '^')
 
-partOneTests =
-  [ (".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n", 21)
-  ]
+input = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n"
+
+partOneTests = [(input, 21)]
+
+partTwoTests = [(input, 40)]
 
 splitBeam :: [Int] -> Int -> [Int]
 splitBeam splitters beam
@@ -36,7 +43,30 @@ splitBeams (acc, beams) splitters = (acc + num_splits, (nub . concat) splits)
     splits = map (splitBeam splitters) beams
     num_splits = length $ filter (== 2) $ map length splits
 
+recursiveSplit :: IORef SplitMap -> Int -> Int -> [[Int]] -> IO Int
+recursiveSplit _ _ _ [] = return 1
+recursiveSplit ref_splitmap level beam (splitters : rest) = do
+  splitmap <- readIORef ref_splitmap
+  case HMS.lookup (beam, level) splitmap of
+    Just stored -> return stored
+    Nothing ->
+      if beam `elem` splitters
+        then do
+          count_left <- recursiveSplit ref_splitmap next_level (beam - 1) rest
+          count_right <- recursiveSplit ref_splitmap next_level (beam + 1) rest
+          let current_count = count_left + count_right
+          modifyIORef ref_splitmap (HMS.insert (beam, level) current_count)
+          return current_count
+        else recursiveSplit ref_splitmap next_level beam rest
+  where
+    next_level = level + 1
+
 partOne :: Input -> Int
 partOne (start, rows) = num_splits
   where
     (num_splits, _) = foldl' splitBeams (0, [start]) rows
+
+partTwo :: Input -> Int
+partTwo (start, rows) = unsafePerformIO $ do
+  splitmap <- newIORef HMS.empty
+  recursiveSplit splitmap 0 start rows
