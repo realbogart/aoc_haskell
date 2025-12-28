ff6312245db113c3fed268614d00e53bcf94c473
Johan Yngman <johan.yngman@gmail.com>
2025-12-28 13:02:36 +0100

    Solved Y2025 D10 part 1

--- src/Y2025/D10.hs
+++ src/Y2025/D10.hs
@@ -5,19 +5,25 @@ import Data.Bits
 
 default (Text, Int)
 
+type Button = Int
+
+type Indicators = Int
+
+type Joltage = Int
+
 data Machine = Machine
-  { indicators :: Int,
-    buttons :: [Int],
-    joltage :: [Int]
+  { indicators_target :: Indicators,
+    buttons :: [Button],
+    joltage :: [Joltage]
   }
   deriving (Show)
 
-indicatorBits :: [Char] -> Int
+indicatorBits :: [Char] -> Indicators
 indicatorBits cs = foldl' (\acc (i, c) -> if c == '#' then setBit acc i else acc) 0 indices
   where
     indices = zip [0 .. length cs - 1] cs
 
-buttonBits :: [Int] -> Int
+buttonBits :: [Int] -> Button
 buttonBits = foldl' setBit 0
 
 parseMachine :: Parser Machine
@@ -36,9 +42,30 @@ parseMachine = do
 parseInput :: Parser [Machine]
 parseInput = parseLineSeparated parseMachine
 
+input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
+
 partOneTests =
   [ ("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", 7)
   ]
 
+pressButton :: Indicators -> Button -> Indicators
+pressButton = xor
+
+pressButtons :: [Button] -> Indicators -> [Indicators]
+pressButtons buttons indicators = map (pressButton indicators) buttons
+
+nextIndicators :: [Button] -> [Indicators] -> [Indicators]
+nextIndicators buttons = concatMap (pressButtons buttons)
+
+findLeastPresses_ :: [Button] -> Indicators -> [Indicators] -> Int -> Int
+findLeastPresses_ buttons indicators_target all_indicators presses
+  | indicators_target `elem` all_indicators_after_presses = presses + 1
+  | otherwise = findLeastPresses_ buttons indicators_target all_indicators_after_presses (presses + 1)
+  where
+    all_indicators_after_presses = nextIndicators buttons all_indicators
+
+findLeastPresses :: Machine -> Int
+findLeastPresses (Machine indicators_target buttons _) = findLeastPresses_ buttons indicators_target [0] 0
+
 partOne :: [Machine] -> Int
-partOne _ = 5
+partOne = sum . map findLeastPresses
