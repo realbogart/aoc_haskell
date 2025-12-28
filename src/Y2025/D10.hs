caf15aced9a27ace7803622798807e0b3aabef31
Johan Yngman <johan.yngman@gmail.com>
2025-12-28 11:59:01 +0100

    Parse Y2025 D10

--- src/Y2025/D10.hs
+++ src/Y2025/D10.hs
@@ -1,8 +1,7 @@
 module Y2025.D10 where
 
 import AoC
-
--- import Data.Bits
+import Data.Bits
 
 default (Text, Int)
 
@@ -11,9 +10,28 @@ data Machine = Machine
     buttons :: [Int],
     joltage :: [Int]
   }
+  deriving (Show)
+
+indicatorBits :: [Char] -> Int
+indicatorBits cs = foldl' (\acc (i, c) -> if c == '#' then setBit acc i else acc) 0 indices
+  where
+    indices = zip [0 .. length cs - 1] cs
+
+buttonBits :: [Int] -> Int
+buttonBits = foldl' setBit 0
 
 parseMachine :: Parser Machine
-parseMachine = return $ Machine 1 [] []
+parseMachine = do
+  indicators <- indicatorBits <$> (char '[' *> (some (char '.' <|> char '#') <* string "] "))
+  buttons <- some parseButton
+  joltage <- parseJoltage
+  return $ Machine indicators buttons joltage
+  where
+    parseButton :: Parser Int
+    parseButton = buttonBits <$> (char '(' *> some (parseInteger <* optional (char ',')) <* string ") ")
+
+    parseJoltage :: Parser [Int]
+    parseJoltage = char '{' *> some (parseInteger <* optional (char ',')) <* char '}'
 
 parseInput :: Parser [Machine]
 parseInput = parseLineSeparated parseMachine
